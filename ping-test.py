88c8e2edd0c7865f5a48d979c0c799061fdf6ac1
matthew <matt.ngai12@gmail.com>
2025-11-17 02:36:30 -0800

    visualize transducer signal (transmit and receive) with one reflector

---
+++ ping-test.py
@@ -0,0 +1,43 @@
+from ping import *
+
+ping_object = Ping(
+    frequency=1e3,
+    duration=0.01,
+    amplitude=100.0,
+    phase=0,
+    sample_rate=4e6,
+    window_mode='gaussian'
+)
+
+transducer_object = Transducer(
+    position=np.array([0.0, 0.0, 0.0]),
+    angle_deg=0.0,
+    beam_width_deg=25.0
+)
+
+reflectors = [
+    Reflector(
+        position=np.array([0.0, 0.0, 10]),
+        radius=0.5,
+        reflectivity=0.8,
+        phase_shift_deg=180.0
+    ),
+]
+
+simulated_world = World(
+    transducer=transducer_object,
+    reflectors=reflectors,
+)
+
+simulated_world.run_simulation(ping_object)
+
+history = transducer_object.transducer_history
+time_axis = np.arange(len(history)) / ping_object.fs
+
+plt.figure(figsize=(12, 5))
+plt.plot(time_axis, history)
+plt.title("Transducer Signal History (Transmit -> Pause -> Echo)")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude (Pa)")
+plt.grid()
+plt.show()
